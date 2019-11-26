dafd5091ec84be8877f8fb800ebae57a1b00efb5
ebentz73 <evanbentz.dev@gmail.com>
2019-11-25 22:46:55 -0500

    Updates to login page, use useStyletron

--- src/components/pages/login.tsx
+++ src/components/pages/login.tsx
@@ -1,46 +1,16 @@
 // @flow
 import React, {useState} from 'react';
-import {Block} from 'baseui/block';
 import {Input} from 'baseui/input';
-import {Button} from 'baseui/button';
+import {Button, KIND} from 'baseui/button';
 import {useMutation, useQuery} from '@apollo/react-hooks';
 import {Redirect} from 'fusion-plugin-react-router';
-import {styled} from 'baseui';
+import {useStyletron} from 'baseui';
 import {SignupUserMutation, LoginUserMutation} from '../mutations';
 import {SessionQuery, SessionQueryType} from '../queries';
-
-const Container = styled('div', ({$theme: {sizing: {scale1200}}}) => ({
-  height: '100%',
-  width: '500px',
-  marginLeft: 'auto',
-  marginRight: 'auto',
-  marginTop: scale1200,
-  backgroundColor: '#FFFFFF',
-}));
-
-const Title = styled(
-  'div',
-  ({
-    $theme: {
-      sizing: {scale800},
-      typography: {font650},
-    },
-  }) => ({
-    ...font650,
-    textAlign: 'center',
-    marginBottom: scale800,
-  })
-);
-
-const StyledButton = styled(Button, ({$theme: {sizing: {scale4800}}}) => ({
-  width: scale4800,
-}));
-
-const Label = styled('div', ({$theme: {colors: {primary500}}}) => ({
-  color: primary500,
-}));
+import {FormControl} from 'baseui/form-control';
 
 export const Login = () => {
+  const [css, theme] = useStyletron();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -58,34 +28,63 @@ export const Login = () => {
     return <Redirect to="/" />;
   }
 
+  const containerStyle = css({
+    height: '100%',
+    width: '500px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: theme.sizing.scale1200,
+    backgroundColor: '#FFFFFF',
+  });
+
+  const titleStyle = css({
+    ...theme.typography.font650,
+    textAlign: 'center',
+    marginBottom: theme.sizing.scale800,
+  });
+
+  const errorStyle = css({
+    marginBottom: theme.sizing.scale400,
+    color: theme.colors.negative,
+    ...theme.typography.font250,
+  });
+
+  const buttonStyle = {
+    width: theme.sizing.scale4800,
+    marginLeft: theme.sizing.scale400,
+  };
+
   return (
-    <Container>
-      <Title>Log in</Title>
-      <Block marginBottom="scale400">
-        <Label>Email</Label>
+    <div className={containerStyle}>
+      <div className={titleStyle}>Log in</div>
+      <FormControl label="Email">
         <Input value={email} onChange={(e: any) => setEmail(e.target.value)} />
-      </Block>
-      <Block marginBottom="scale400">
-        <Label>Password</Label>
+      </FormControl>
+      <FormControl label="Password">
         <Input
           value={password}
           onChange={(e: any) => setPassword(e.target.value)}
           type="password"
         />
-      </Block>
-      {session.error && (
-        <Block marginBottom="scale400" color="negative">
-          {session.error}
-        </Block>
-      )}
-      <Block display="flex" justifyContent="flex-end">
-        <StyledButton onClick={() => signup()} isLoading={signupLoading}>
+      </FormControl>
+      {session.error && <div className={errorStyle}>{session.error}</div>}
+      <div className={css({display: 'flex', justifyContent: 'flex-end'})}>
+        <Button
+          kind={KIND.secondary}
+          onClick={() => signup()}
+          isLoading={signupLoading}
+          $style={buttonStyle}
+        >
           Sign Up
-        </StyledButton>
-        <StyledButton onClick={() => login()} isLoading={loginLoading}>
+        </Button>
+        <Button
+          onClick={() => login()}
+          isLoading={loginLoading}
+          $style={buttonStyle}
+        >
           Login
-        </StyledButton>
-      </Block>
-    </Container>
+        </Button>
+      </div>
+    </div>
   );
 };
